1b76b1595a2aec839896286537a13baf5efb2130
Vasilya <lady_drakula_94@mail.ru>
2022-09-21 15:47:13 +0500

    Add file Exersize003

--- Exersize001/Program.cs
+++ Exersize001/Program.cs
@@ -2,6 +2,7 @@
 int a = -9;
 int b = -3;
 int max = a;
+if (a > max) max = a;
 if (b > max) max = b;
 Console.Write("max = ");
 Console.WriteLine(max);
---
+++ Exersize003/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Определить чётность и нечетность числа");
+int a = 15;
+Console.Write(a);
+if (a%2 == 0) 
+    Console.WriteLine(" - Четное");
+    else
+    Console.WriteLine("- Нечетное "); 
