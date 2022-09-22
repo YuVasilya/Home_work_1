bb0d38554b1f6051af245b84e83c57b2d822df5f
Vasilya <lady_drakula_94@mail.ru>
2022-09-22 10:44:52 +0500

    Changed file Exersize001

--- Exersize001/Program.cs
+++ Exersize001/Program.cs
@@ -1,6 +1,10 @@
-﻿// Программа ищет максимальное число из двух данных
-int a = -9;
-int b = -3;
+﻿Console.WriteLine("Программа ищет максимальное число из двух данных");
+int a;
+Console.Write("Number a = ");
+a = Convert.ToInt32(Console.ReadLine());
+int b;
+Console.Write("Number b = ");
+b = Convert.ToInt32(Console.ReadLine());
 int max = a;
 if (a > max) max = a;
 if (b > max) max = b;
